edfbb8d3b54b1679d85220b117df99c4b79b6de7
Oliver Bo Wolter Nielsen <oni@vucfyn.dk>
2017-11-10 10:36:39 +0100

    added clozetest

---
+++ public/js/clozetest_participant.js
@@ -0,0 +1,223 @@
+{
+    var totalLineCount;
+    var count;
+    var testlist;
+    var audioCount = 0;
+    var d = new Date();
+    var startTime;
+    var checkpoint;
+
+    function initializeTest(testlist) {
+
+        this.testlist = testlist;
+
+        totalLineCount = testlist.lines.length;
+        count = 0;
+
+        $('#start').click(function () {
+            startTime = d.getTime();
+            checkpoint = startTime;
+            console.log(startTime);
+
+            nextLine(count);
+            this.remove();
+        });
+
+
+        $audioFile = $('<audio/>').attr({
+            src: '../images/aaaah.wav'
+        });
+
+        $audioControl = $('<input/>').attr({
+            class: 'h2size',
+            type: 'button',
+            id: 'audioControl' + count,
+            value: 'Afspil'
+        }).click(function () {
+            $audioFile[0].play();
+
+            if (audioCount > 0) {
+                this.remove();
+                audioCount = 0;
+            } else {
+                audioCount++;
+            }
+            //playAudio($audioFile);  
+        });
+
+        $('#subsection').prepend($audioControl);
+
+    }
+
+
+
+    function nextLine(count) {
+
+        // indsætter det første linjestykke
+        $lineP1 = $('<nobr/>')
+            .attr({
+                class: 'h2size'
+            })
+            .text(testlist.lines[count][0]);
+
+        $lineInput = $('<input/>').attr({
+            class: 'h2size',
+            id: 'input' + count,
+            name: 'userinput',
+            placeholder: 'Indsæt ord'
+        });
+
+
+        $lineP2 = $('<nobr/>')
+            .attr({
+                class: 'h2size'
+            })
+            .text(testlist.lines[count][1]);
+
+
+        /*$audioControl = $('<audio controls></audio>')
+            .append('</source>')
+            .attr({
+                id: 'soundSrc' + count,
+                src: '../images/aaaah.wav'
+            }); 
+
+        $label = $('<label/>').text(testlist.files[count]);*/
+
+
+        $audioFile = $('<audio/>').attr({
+            src: '../images/aaaah.wav'
+        });
+
+        $audioControl = $('<input/>').attr({
+            class: 'h2size',
+            type: 'button',
+            id: 'audioControl' + count,
+            value: 'Afspil'
+        }).click(function () {
+            $audioFile[0].play();
+
+            if (audioCount > 0) {
+                this.remove();
+                audioCount = 0;
+            } else {
+                audioCount++;
+            }
+            //playAudio($audioFile);  
+        });
+
+        $timestamp = $('<input/>').attr({
+            type: 'hidden',
+            id: 'timestamp' + count,
+            name: 'timestamp'
+        });
+
+
+        // tilføjer alle elementer til siden 
+        $('#subsection')
+            .append($lineP1)
+            .append($lineInput)
+            .append($lineP2)
+            .append($audioControl)
+            //            .append($label)
+            .append($timestamp)
+            .append('<br>');
+
+
+
+        $('#input' + count).one('keyup', function () {
+            $nextButton = $('<button/>').attr({
+                class: 'h2size',
+                id: 'button' + count
+            });
+
+            if ((count + 1) < totalLineCount) {
+                $nextButton
+                    .click(function () {
+                        next();
+                    })
+                    .text('Næste');
+                $('#subsubsection').append($nextButton);
+            } else {
+                $submit = $('<input/>').attr({
+                    type: 'submit',
+                    value: 'Gem/Videre'
+                });
+                $('#bottom').append($submit);
+            }
+
+
+        });
+    }
+
+
+    /*function playAudio(audioFile) {
+        audioFile[0].play();
+    }*/
+
+
+    function next() {
+        setTime();
+
+        $('#audioControl' + count).remove();
+        audioCount = 0;
+
+        count++;
+
+        nextLine(count);
+
+        $('#subsubsection').empty();
+    }
+
+
+    function setTime() {
+        var d = new Date();
+        var timestamp = (d.getTime() - checkpoint);
+        console.log(timestamp);
+        $('#timestamp' + count).val(timestamp);
+        checkpoint = d.getTime();
+    }
+
+    // kan måske bruges når der skal tjekkes svar !!
+
+
+    /* $(function() {
+         
+         // når der klikkes, tjekkes svarene 
+         $('#submit').click(function() {
+             checkAnswer();     
+         });
+         
+     });
+     
+     var lineAnswerArray = []; 
+     
+     // tjekker svar
+     function checkAnswer() {
+         
+         for(var i=0; i<numberOfQuestions; i++) {
+             // alle svar bliver sat til små bogstaver, og fjerner mellemrum
+             lineAnswerArray[i] = $('#lineInput' + i).val().toLowerCase().trim();
+             
+             // de korrekte svar bliver ligeledes lavet til små bogstaver, og trimmet
+             var correctAnswer = dataFromParent[1][i].toLowerCase().trim()
+             
+             // sammenligner svar med korrekte svar, og giver feedback 
+             // VIGTIGT - skal fjernes når der kommer database 
+             if(lineAnswerArray[i] == correctAnswer) {
+                 $('#newLine' + i)
+                         .css('backgroundColor','lightGreen')
+                         .append('   : Det korrekte ord: ' + correctAnswer);
+             } else {
+                 $('#newLine' + i)
+                     .css('backgroundColor', 'lightGrey')
+                     .append('   : Det korrekte ord: ' + correctAnswer); 
+             }
+         }
+         
+         
+     }
+     */
+
+
+}
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
-router.get('/filepicker', function(req, res, next) {
-    res.render('filepicker', {title: 'Filepicker' }); 
-}); 
-        
+router.get('/filepicker', function (req, res, next) {
+    res.render('filepicker', {
+        title: 'Filepicker'
+    });
+});
 
-        /*      HER ER ALLE TEACHER SIDERNE       */
+var initials = "oni";
+/*      HER ER ALLE TEACHER SIDERNE       */
 
 
-        /* ALLE FUNKTIONER DER ER TILKNYTTET MAIN */
+/* ALLE FUNKTIONER DER ER TILKNYTTET MAIN */
 
 //henter hjemmesiden 'main' 
-router.get('/main', function(req, res) {
+/*router.get('/main', function(req, res) {
     res.render('main', { title: 'Main' });
 });
 
@@ -45,39 +49,40 @@ router.post('/main_addinfo', function(req, res) {
             res.redirect("worddictate_teacher");
         }
     });
-});
+});*/
 
 
 
-        /* ALLE FUNKTIONER DER ER TILKNYTTET WORDDICTATE */
+/* ALLE FUNKTIONER DER ER TILKNYTTET WORDDICTATE */
 
 //henter hjemmesiden 'worddictate_teacher' 
-router.get('/worddictate_teacher', function(req, res) {
-    res.render('worddictate_teacher', { title: 'Orddiktat' });
+router.get('/worddictate_teacher', function (req, res) {
+    res.render('worddictate_teacher', {
+        title: 'Orddiktat'
+    });
 });
 
 
-router.post('/worddictate_addinfo', function(req, res) {
+router.post('/worddictate_addinfo', function (req, res) {
     // Set our internal DB variable
     var db = req.db;
     // Get our form values. These rely on the "name" attributes 
     var lines = req.body.lines;
-    var files = req.body.files; 
-    
+    var files = req.body.files;
+
     // Set our collection
     var collection = db.get('worddictate');
 
     // Submit to the DB
     collection.insert({
-        "initials" : initials,
-        "lines" : lines,
-        "files" : files
+        "initials": initials,
+        "lines": lines,
+        "files": files
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
             res.send("There was a problem adding the information to the database.");
-        }
-        else {
+        } else {
             // And forward to success page
             res.redirect("nonsense_teacher");
         }
@@ -86,42 +91,84 @@ router.post('/worddictate_addinfo', function(req, res) {
 
 
 
-    /* ALLE FUNKTIONER DER ER TILKNYTTET NONSENSE*/
+/* ALLE FUNKTIONER DER ER TILKNYTTET NONSENSE*/
 
-router.get('/nonsense_teacher', function(req, res) {
-    res.render('nonsense_teacher', { title: 'Vrøvleord' });
+router.get('/nonsense_teacher', function (req, res) {
+    res.render('nonsense_teacher', {
+        title: 'Vrøvleord'
+    });
 });
 
 
-router.post('/nonsense_addinfo', function(req, res) {
-    var db = req.db; 
-    
-    var files = req.body.files; 
-    
-    var collection = db.get('nonsense'); 
-    
+router.post('/nonsense_addinfo', function (req, res) {
+    var db = req.db;
+
+    var files = req.body.files;
+
+    var collection = db.get('nonsense');
+
     collection.insert({
-        "initials" : initials,
-        "files" : files
-    }, function(err, doc) {
-        if(err) {
+        "initials": initials,
+        "files": files
+    }, function (err, doc) {
+        if (err) {
             res.send("There was a problem adding the information to the database.");
         } else {
-            res.redirect("nextpage");
+            res.redirect("clozetest_teacher");
         }
-    }); 
+    });
 });
 
 
 
-    /* NEXTPAGE ER EN DUMMY DER SENDER DIG VIDERE TIL KURSISTSIDEN*/
 
-router.get('/nextpage', function(req, res) {
-    res.render('nextpage', { title: 'Nextpage' });
+/* ALLE FUNKTIONER DER ER TILKNYTTET CLOZETEST*/
+
+router.get('/clozetest_teacher', function (req, res) {
+    res.render('clozetest_teacher', {
+        title: 'Clozetest'
+    });
 });
 
 
-router.post('/nextpage', function(req, res) {
+router.post('/clozetest_addinfo', function (req, res) {
+    // Set our internal DB variable
+    var db = req.db;
+    // Get our form values. These rely on the "name" attributes 
+    var lines = req.body.lines;
+    var files = req.body.files;
+
+    // Set our collection
+    var collection = db.get('clozetest');
+
+    // Submit to the DB
+    collection.insert({
+        "initials": initials,
+        "lines": lines,
+        "files": files
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        } else {
+            // And forward to success page
+            res.redirect("clozetest_participant");
+        }
+    });
+});
+
+
+
+/* NEXTPAGE ER EN DUMMY DER SENDER DIG VIDERE TIL KURSISTSIDEN*/
+
+router.get('/nextpage', function (req, res) {
+    res.render('nextpage', {
+        title: 'Nextpage'
+    });
+});
+
+
+router.post('/nextpage', function (req, res) {
     res.redirect("startpage");
 });
 
@@ -133,129 +180,177 @@ router.post('/nextpage', function(req, res) {
 
 
 
+/*      HER ER ALLE PARTICIPANT SIDERNE     */
 
-        /*      HER ER ALLE PARTICIPANT SIDERNE     */
 
-
-        /* ALLE FUNKTIONER DER ER TILKNYTTET STARTPAGE */
+/* ALLE FUNKTIONER DER ER TILKNYTTET STARTPAGE */
 
 //henter hjemmesiden 'startpage' 
-router.get('/startpage', function(req, res) {
-    res.render('startpage', { title: 'Startside' });
+router.get('/startpage', function (req, res) {
+    res.render('startpage', {
+        title: 'Startside'
+    });
 });
 
 
-router.post('/startpage_addinfo', function(req, res) {
-    var db = req.db; 
-    
-    var date = req.body.date_input; 
+router.post('/startpage_addinfo', function (req, res) {
+    var db = req.db;
+
+    var date = req.body.date_input;
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
-    var dys = req.body.dys_select; 
-    var fam = req.body.fam_select; 
-    var tong = req.body.tong_input; 
-    var home = req.body.home_input; 
-    
-    var collection = db.get('startpage'); 
-    
+    var dys = req.body.dys_select;
+    var fam = req.body.fam_select;
+    var tong = req.body.tong_input;
+    var home = req.body.home_input;
+
+    var collection = db.get('startpage');
+
     collection.insert({
-        
-        "date" : date,
-        "firstname" : firstname, 
-        "lastname" : lastname,
-        "is_dyslexic" : dys, 
-        "is_familyDyslexic" : fam, 
-        "mother_tongue" : tong,
-        "lang_at_home" : home
-        
-    }, function(err, doc) {
-        if(err) {
+
+        "date": date,
+        "firstname": firstname,
+        "lastname": lastname,
+        "is_dyslexic": dys,
+        "is_familyDyslexic": fam,
+        "mother_tongue": tong,
+        "lang_at_home": home
+
+    }, function (err, doc) {
+        if (err) {
             res.send("There was a problem adding the information to the database.");
         } else {
-            res.redirect("worddictate_participant"); 
+            res.redirect("worddictate_participant");
         }
     });
 });
 
 
 
-        /* ALLE FUNKTIONER DER ER TILKNYTTET WORDDICTATE */
+/* ALLE FUNKTIONER DER ER TILKNYTTET WORDDICTATE */
 
 //henter 'worddictate_participant' og finder data i databasen, svarende til de indtastede initialer
-router.get('/worddictate_participant', function(req, res) {
-    var db = req.db; 
-    var collection = db.get('worddictate'); 
-    
+router.get('/worddictate_participant', function (req, res) {
+    var db = req.db;
+    var collection = db.get('worddictate');
+
     //lige nu henter den alle documenter med disse initialer, selvom den kun skal vise 1 (den første)
     //senere skal der tilføjes en hovedside hvor brugeren kan vælge hvilken test, på baggrund af sine initialer 
-    collection.find({'initials' : initials}, {}, function(e, docs) {
-        var docLenght = docs.length; 
+    collection.find({
+        'initials': initials
+    }, {}, function (e, docs) {
+        var docLenght = docs.length;
         res.render('worddictate_participant', {
-            "testlist" : docs[docLenght-1],
+            "testlist": docs[docLenght - 1],
             title: 'worddictate_participant'
         });
     });
-}); 
+});
 
 
-router.post('/worddictate_addanswer', function(req, res) {
-    var db = req.db; 
-    
+router.post('/worddictate_addanswer', function (req, res) {
+    var db = req.db;
+
     var userinput = req.body.userinput;
-    var timestamp = req.body.timestamp; 
-    
-    var collection = db.get('worddictate_answer'); 
-    
+    var timestamp = req.body.timestamp;
+
+    var collection = db.get('worddictate_answer');
+
     collection.insert({
-        "participant_answer" : userinput,
-        "timestamp" : timestamp
-    }, function(err, doc) {
-        if(err) {
+        "participant_answer": userinput,
+        "timestamp": timestamp
+    }, function (err, doc) {
+        if (err) {
             res.send("There was a problem adding the information to the database.");
         } else {
             res.redirect("nonsense_participant");
         }
-    }); 
-}); 
+    });
+});
 
 
 
-    /* ALLE FUNKTIONER DER ER TILKNYTTET NONSENSE */
+/* ALLE FUNKTIONER DER ER TILKNYTTET NONSENSE */
 
 //henter 'output' og finder data i databasen, svarende til de indtastede initialer
-router.get('/nonsense_participant', function(req, res) {
-    var db = req.db; 
-    var collection = db.get('nonsense'); 
-    
+router.get('/nonsense_participant', function (req, res) {
+    var db = req.db;
+    var collection = db.get('nonsense');
+
     //lige nu henter den alle documenter med disse initialer, selvom den kun skal vise 1 (den første)
     //senere skal der tilføjes en hovedside hvor brugeren kan vælge hvilken test, på baggrund af sine initialer 
-    collection.find({'initials' : initials}, {}, function(e, docs) {
-        var docLenght = docs.length; 
+    collection.find({
+        'initials': initials
+    }, {}, function (e, docs) {
+        var docLenght = docs.length;
         res.render('nonsense_participant', {
-            "testlist" : docs[docLenght-1],
+            "testlist": docs[docLenght - 1],
             title: 'nonsense_participant'
         });
     });
-}); 
+});
 
 
-router.post('/nonsense_addanswer', function(req, res) {
-    var db = req.db; 
-    
-    var userinput = req.body.userinput; 
-    
-    var collection = db.get('nonsense_answer'); 
-    
+router.post('/nonsense_addanswer', function (req, res) {
+    var db = req.db;
+
+    var userinput = req.body.userinput;
+
+    var collection = db.get('nonsense_answer');
+
     collection.insert({
-        "participant_answer" : userinput
-    }, function(err, doc) {
-        if(err) {
+        "participant_answer": userinput
+    }, function (err, doc) {
+        if (err) {
+            res.send("There was a problem adding the information to the database.");
+        } else {
+            res.redirect("clozetest_participant");
+        }
+    });
+});
+
+
+
+/* ALLE FUNKTIONER DER ER TILKNYTTET CLOZETEST */
+
+//henter clozetest_participant og finder data i databasen, svarende til de indtastede initialer
+router.get('/clozetest_participant', function (req, res) {
+    var db = req.db;
+    var collection = db.get('clozetest');
+
+    //lige nu henter den alle documenter med disse initialer, selvom den kun skal vise 1 (den første)
+    //senere skal der tilføjes en hovedside hvor brugeren kan vælge hvilken test, på baggrund af sine initialer 
+    collection.find({
+        'initials': initials
+    }, {}, function (e, docs) {
+        var docLenght = docs.length;
+        res.render('clozetest_participant', {
+            "testlist": docs[docLenght - 1],
+            title: 'clozetest_participant'
+        });
+    });
+});
+
+
+router.post('/clozetest_addanswer', function (req, res) {
+    var db = req.db;
+
+    var userinput = req.body.userinput;
+    var timestamp = req.body.timestamp;
+
+    var collection = db.get('clozetest_answer');
+
+    collection.insert({
+        "participant_answer": userinput,
+        "timestamp": timestamp
+    }, function (err, doc) {
+        if (err) {
             res.send("There was a problem adding the information to the database.");
         } else {
             res.redirect("finalpage");
         }
-    }); 
-}); 
+    });
+});
 
 
 
