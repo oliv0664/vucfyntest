e2a993cad3e9f7265bcd3721f1498e1f2d95b0f5
oliv0664 <oni@vucfyn.dk>
2018-03-01 15:21:34 +0100

    ups ups igen...

--- public/js/startpage.js
+++ public/js/startpage.js
@@ -24,8 +24,8 @@ $(function() {
 
         $(this).unbind('submit').submit();
 
-        // console.log($('#eg_con').val());
-        // console.log($('#eg_read_write_in_job').val()); 
+        console.log($('#eg_con').val());
+        console.log($('#eg_read_write_in_job').val()); 
     }); 
 
 
