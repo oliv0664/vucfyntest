73d19da4bd63ece8b3c3741b0182a26bd0f0cb5b
oliv0664 <oni@vucfyn.dk>
2018-07-12 12:10:52 +0200

    trying Google Cloud Platform deplaoyment

--- public/js/MongoHandler.js
+++ public/js/MongoHandler.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://localhost/vucfyntest';
+// var mongoDB = 'mongodb://localhost/vucfyntest';
+var mongoDB = 'mongodb://vucfyntest:test@ds237475.mlab.com:37475/vucfyntestdb';
+
 var Grid = require('gridfs-stream');
 var fs = require('fs');
  
