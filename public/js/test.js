6e32ead12954335308769ad6d98060632d582eb8
Luca Casa <lc301092@gmail.com>
2018-07-23 09:35:06 +0200

    oversigt og lidt ekstra

--- public/js/test.js
+++ public/js/test.js
@@ -1,14 +1,28 @@
-{
-    
-    
-    function initializeTest(testList) {
-        
-        console.log(testList[0].title); 
-        
-        $('#main').append('<p>'+testList[0].title+'</p>'); 
 
-    }
-    
-    
-    
+$(function () {
+	console.log('ready');
+	$('.ul_btn').click(function () {
+		var x = $(this).siblings("ul:first");
+		console.log(x);
+		x.toggle();
+		if ($(this).html() == '+') {
+			$(this).html('-');
+		} else {
+			$(this).html('+');
+		}
+
+	});
+	$('.seeAnswer').click(function(){
+		var val = $(this).val();	
+	alert(val + " skal sendes med videre til næste side");
+	});
+});
+
+function newTest() {
+	window.location.assign("index");
+}
+
+function initializeTest(testList) {
+	console.log(testList[0].title);
+	$('#main').append('<p>' + testList[0].title + '</p>');
 }
