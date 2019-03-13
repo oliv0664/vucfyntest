584b840dd676c5220de9e4d821b220f675de2de6
Luca Casa <lca@vucfyn.dk>
2019-03-13 09:19:57 +0100

    Update old_hag.js
    uuuhh automatisk

--- public/js/old_hag.js
+++ public/js/old_hag.js
@@ -1,14 +1,17 @@
-  function formSetup(myFunc){
-        $('#form').bind('submit', function(event) {
-            event.preventDefault();
-            // ORDDIKTAT ER DU NÅET TIL
-            $('#form input[type = submit]').remove();
-            //this will prevent the default submit
-            console.log('running form setup', myFunc);
-            if(myFunc){
-            myFunc();
-            }
+  function formSetup(myFunc) {
+      $('#form').bind('submit', function (event) {
+          event.preventDefault();
+          // ORDDIKTAT ER DU NÅET TIL
+          if (confirm('Hvis du går videre kan du ikke komme tilbage')) {
+              $('#form input[type = submit]').remove();
+              //this will prevent the default submit
+              console.log('running form setup', myFunc);
 
-            $(this).unbind('submit').submit(); // continue the submit unbind preventDefault
-        });
-    }
+              if (myFunc) {
+                  myFunc();
+              }
+          $(this).unbind('submit').submit(); // continue the submit unbind preventDefault
+          }
+
+      });
+  }
